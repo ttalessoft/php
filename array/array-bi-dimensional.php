de9e6a7fd083d5eb88a65f444f3a03621c693e65
ttalessoft <ttalessoft@gmail.com>
2018-11-30 18:46:07 -0200

    arrays multidimensionais

---
+++ array/array-bi-dimensional.php
@@ -0,0 +1,12 @@
+<?php
+
+    $carros[0][0] = 'ferrari';
+    $carros[0][1] = 'fiat';
+    $carros[0][2] = 'lamborguini';
+    $carros[1][0] = 'diablo';
+    $carros[0][3] = 'ford';
+    $carros[1][1] = 'volkswagen';
+    $carros[0][4] = 'punto';
+    $carros[1][2] = 'audi';
+
+    print_r($carros);
