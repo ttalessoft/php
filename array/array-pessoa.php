367a26bbe25ce88ae86dbf7997daf079d375b71b
ttalessoft <ttalessoft@gmail.com>
2018-11-30 19:12:54 -0200

    trabalhando com arrays

---
+++ array/array-pessoa.php
@@ -0,0 +1,12 @@
+<?php
+
+$pessoaFisica = array();
+
+array_push($pessoasFisica, array(
+    'nome'=>'ttales',
+    'docId'=>'999.999.999-99',
+    'doc'=>'999.999.999-9',
+    'dt'=>'2018-08-08'
+));
+
+var_dump($pessoaFisica);
